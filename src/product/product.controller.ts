02694c96c4804f9d01b7ffaf698e17f5039b40ee
hashmi mohammed <hashmi.smf@gmail.com>
2023-11-29 20:28:30 +0530

    checked adding custom get api method

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
-@Controller('product')
-export class ProductController {}
+@Controller('/products')
+export class ProductController {
+  @Get()
+  products() {
+    return [
+      {
+        id: 1,
+        name: 'product 1',
+        description: 'product 1',
+        price: 100,
+      },
+      {
+        id: 2,
+        name: 'product 2',
+        description: 'product 2',
+        price: 200,
+      },
+      {
+        id: 3,
+        name: 'product 3',
+        description: 'product 3',
+        price: 300,
+      },
+      {
+        id: 500,
+        name: 'india product',
+        description: 'india product',
+        price: 500,
+      },
+      {
+        id: 600,
+        name: 'india product',
+        description: 'india product',
+        price: 600,
+      },
+    ];
+  }
+}
