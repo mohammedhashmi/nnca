e8242efd14b23a83bb5b7aa9973d98cd4d307d6f
hashmi mohammed <hashmi.smf@gmail.com>
2023-12-05 13:29:31 +0530

    connected to local mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, ProductModule],
+  imports: [
+    UserModule,
+    ProductModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'test',
+      password: 'test12345',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
